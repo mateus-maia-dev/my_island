003f316d38ceddef7cb2d67dd113acfeb08e65d3
mateusmaiaf <eng.fonsecamateus@gmail.com>
2021-08-05 22:25:06 -0300

    first commit

---
+++ src/static/places.js
@@ -0,0 +1,17 @@
+const places = [
+    {
+        title: 'Fernando de Noronha',
+        description: "A ilha de Fernando de Noronha é considerada um dos parísos brasileiros. Com uma fauna e flora riquíssimas! Tem o nome da sua maior ilha, um parque marinho e santuário ecológico protegido.",
+        imageUrl: process.env.PUBLIC_URL + '/assets/photo1.jpg',
+        time: 1500
+    },
+    {
+        title: 'Angra dos Reis',
+        description: "Angra dos Reis é um município brasileiro no estado de Rio de Janeiro, a sudoeste. É composto por 365 ilhas e um pequeno porto rodeado por uma linha costeira íngreme e arborizada! ",
+        imageUrl: process.env.PUBLIC_URL + '/assets/photo2.jpg',
+        time: 1500
+    }
+
+]
+
+export default places
